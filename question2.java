b29707903b5674dc292e570a3bcdc7c3722c44f6
Surya Krishna <suryak614@gmail.com>
2023-05-24 19:06:20 +0530

    question 1 to 4 done

---
+++ question2.java
@@ -0,0 +1,20 @@
+public class question2 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        
+        num1 += 2; 
+        System.out.println("After addition: num = " + num1);
+        
+        num1 -= 4; 
+        System.out.println("After subtraction: num = " + num1);
+        
+        num1 *= 7; 
+        System.out.println("After multiplication: num = " + num1);
+        
+        num1 /= 4; 
+        System.out.println("After division: num = " + num1);
+        
+        num1 %= 12; 
+        System.out.println("After modulus: num = " + num1);    
+    }
+}
